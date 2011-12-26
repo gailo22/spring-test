bfc3d0cef61e3d98cf74201bbbb47b178cd321d0
gailo22 <gailo22@gmail.com>
2011-12-26 22:55:27 +0800

    udpated

--- src/spring/test/rest/RestTemplateTest.java
+++ src/spring/test/rest/RestTemplateTest.java
@@ -25,14 +25,14 @@ public class RestTemplateTest {
         
         // GET customer
         url = BASE_URL + "/customer/{id}";
-        str = restTemplate.getForObject(url, String.class, "1234");
+        str = restTemplate.getForObject(url, String.class, "1");
         System.out.println(str);
         
         // POST
         url = BASE_URL + "/customer";
         Customer customer = new Customer();
-        customer.setId("1234567890");
-        customer.setName("Montree134");
+        customer.setId("22");
+        customer.setName("Montree " + customer.getId());
         URI newCustomertLocation = restTemplate.postForLocation(url, customer);
         System.out.println(newCustomertLocation);
         
